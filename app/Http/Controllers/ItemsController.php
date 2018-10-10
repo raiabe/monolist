e66c8a12d4d47af4c2e28ef18353b32e90ededbb
raiabe <gunyaa1441@gmail.com>
2018-10-10 06:35:44 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -37,4 +37,16 @@ use Illuminate\Http\Request;
             'items' => $items,
         ]);
     }
+    
+    
+        public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
   }
